d12642f27ad75a5456546dae5cef70851b1e9d2a
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2026-04-04 14:27:01 +0500

    fix: Swagger docs path for production

--- backend/config/swaggerAdmin.js
+++ backend/config/swaggerAdmin.js
@@ -1,4 +1,5 @@
 const swaggerJsdoc = require('swagger-jsdoc');
+const path = require('path');
 
 const options = {
   definition: {
@@ -69,7 +70,11 @@ const options = {
       },
     ],
   },
-  apis: ['./routes/*.js', './docs/swagger/*.js', './index.js'],
+  apis: [
+    path.join(__dirname, '../routes/*.js'),
+    path.join(__dirname, '../docs/swagger/*.js'),
+    path.join(__dirname, '../index.js')
+  ],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
